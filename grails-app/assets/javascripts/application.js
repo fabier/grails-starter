6a6555ac1d2c7a4a434b937b8cdf3d497e52a673
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-14 15:11:20 +0100

    Intégration de bootstrap twitter au format LESS
    Ajout des styles par défaut :
    common.less
    main.less
    Passage de l'ensemble au format LESS

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,6 +5,7 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better 
 // to create separate JavaScript files as needed.
 //
+//= require bootstrap
 //= require jquery
 //= require_tree .
 //= require_self
