6233145630c695d52cc9ac047d586d5931b064f8
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-14 10:30:01 +0100

    Suppression du scaffolding Grails par défaut

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -63,7 +63,7 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.55.2" // or ":tomcat:8.0.20"
 
         // plugins for the compile step
-        compile ":scaffolding:2.1.2"
+        // compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile ":asset-pipeline:2.1.5"
 
