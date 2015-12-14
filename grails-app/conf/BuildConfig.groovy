6e6874fce8b721e33490ccfc82ff4b95242fe107
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-14 10:35:08 +0100

    Ajout de Spring Security (pour l'authentification des utilisateurs)

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -76,5 +76,9 @@ grails.project.dependency.resolution = {
 
         // Bootstrap
         compile ":twitter-bootstrap:3.3.4"
+
+        // Gestion utilisateurs
+        compile ":spring-security-core:2.0-RC4"
+        compile ":spring-security-ui:1.0-RC2"
     }
 }
