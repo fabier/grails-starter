dabfbd4af8c7e6c1811ba52c1161156abb2c60ee
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-14 10:33:41 +0100

    Ajout de jQuery UI

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -72,6 +72,7 @@ grails.project.dependency.resolution = {
         runtime ":hibernate4:4.3.8.1" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
+        compile ":jquery-ui:1.10.4"
 
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
