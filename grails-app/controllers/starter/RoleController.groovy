5cf305d69bbbeeadb9e0f02818837c394eec9eb0
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-14 17:57:44 +0100

    BUGFIX sur RoleController

--- grails-app/controllers/starter/RoleController.groovy
+++ grails-app/controllers/starter/RoleController.groovy
@@ -6,6 +6,8 @@ import org.springframework.security.access.annotation.Secured
 class RoleController extends grails.plugin.springsecurity.ui.RoleController {
 
     def search() {
+        setIfMissing 'max', 10, 100
+        setIfMissing 'offset', 0
         def results = Role.createCriteria().list {
             order("authority", "asc")
             maxResults(10)
