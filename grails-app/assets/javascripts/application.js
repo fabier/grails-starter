0bf2bfa4d8f1190b8a8e96df1a319ce1b6de7fc1
Pierre FABIER <pierre.fabier@gmail.com>
2015-12-15 13:19:16 +0100

    Modification de la position de jQuery dans l'ordre des imports
    Permet à bootstrap de se charger correctement

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -5,8 +5,8 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better 
 // to create separate JavaScript files as needed.
 //
-//= require bootstrap
 //= require jquery
+//= require bootstrap
 //= require_tree .
 //= require_self
 
